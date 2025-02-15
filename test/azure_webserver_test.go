d2ee957cdbda31b1c4e4ae6746c86a0e85115c1b
shivareddysama <sama0096@algonquinlive.com>
2025-02-14 21:28:08 -0500

    Run initial tests

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "fdcbdc66-325a-470d-957c-7977b0fb7718"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "sama0096",
 		},
 	}
 
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "labelPrefix" {
 }
 
 variable "region" {
-  default = "westus3"
+  default = "centralus"
 }
 
 variable "admin_username" {
