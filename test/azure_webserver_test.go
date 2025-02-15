a29a1467ea7c3fc08689e10c0c27093448d389ba
shivareddysama <sama0096@algonquinlive.com>
2025-02-14 22:23:22 -0500

    Add NIC and Ubuntu version tests

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -2,21 +2,18 @@ package test
 
 import (
 	"testing"
+	"strings"
 
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
-// You normally want to run this under a separate "Testing" subscription
-// For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
 var subscriptionID string = "fdcbdc66-325a-470d-957c-7977b0fb7718"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
 		TerraformDir: "../",
-		// Override the default terraform variables
 		Vars: map[string]interface{}{
 			"labelPrefix": "sama0096",
 		},
@@ -24,13 +21,35 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	// Run `terraform init` and `terraform apply`
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of output variable
+	// Get output variables
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	nicName := terraform.Output(t, terraformOptions, "nic_name")
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// 1. Confirm NIC exists and is connected to VM
+	assert.True(t, azure.NetworkInterfaceExists(t, nicName, resourceGroupName, subscriptionID), "NIC should exist")
+
+	vm := azure.GetVirtualMachine(t, vmName, resourceGroupName, subscriptionID)
+	connectedNICs := vm.NetworkProfile.NetworkInterfaces
+
+	isNicConnected := false
+	for _, nic := range *connectedNICs {  // Dereference the pointer here
+		if strings.Contains(*nic.ID, nicName) {
+			isNicConnected = true
+			break
+		}
+	}
+	assert.True(t, isNicConnected, "NIC should be connected to the VM")
+
+	// 2. Confirm the VM is running the correct Ubuntu version
+	expectedOSVersion := "0001-com-ubuntu-server-jammy"  // Adjust to your expected version, e.g., "Ubuntu 20.04 LTS"
+	actualOSVersion := *vm.StorageProfile.ImageReference.Offer
+	t.Logf("Actual OS Version: %s", actualOSVersion)
+	assert.True(t, strings.Contains(actualOSVersion, expectedOSVersion), "VM should be running the expected Ubuntu version")
 }
